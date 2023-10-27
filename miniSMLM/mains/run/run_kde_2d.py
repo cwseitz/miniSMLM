5cf309fa61a1bc96408582eac4a27fe9bee0b726
cwseitz <cwseitz2@gmail.com>
2023-10-27 19:18:26 -0400

    update

--- miniSMLM/mains/run/run_kde_2d.py
+++ miniSMLM/mains/run/run_kde_2d.py
@@ -9,27 +9,14 @@ from skimage.io import imsave
 with open('run_kde_2d.json', 'r') as f:
     config = json.load(f)
 
-prefixes = [
-'230909_Hela_j646_10pM_overnight_2000frames_20mW_fixed-1',
-'230909_Hela_j646_10pM_overnight_2000frames_20mW_fixed-3',
-'230909_Hela_j646_10pM_overnight_2000frames_20mW_fixed-5',
-'230909_Hela_j646_10pM_overnight_2000frames_20mW_fixed-8',
-'230909_Hela_j646_10pM_overnight_2000frames_20mW_fixed-9',
-'230909_Hela_j646_10pM_overnight_2000frames_20mW_live-1',
-'230909_Hela_j646_10pM_overnight_2000frames_20mW_live-3',
-'230909_Hela_j646_10pM_overnight_2000frames_20mW_live-4',
-'230909_Hela_j646_10pM_overnight_2000frames_20mW_live-8'
-]
-
+prefixes = []
 
 plot=False
 for n,prefix in enumerate(prefixes):
     print("Processing " + prefix)
-    spots = pd.read_csv(config['ipath'] + prefix + '_spots.csv')
-    spots = spots.loc[(spots['N0'] > 10) & (spots['N0'] < 5000) & (spots['x_mle'] > 0) & (spots['y_mle'] > 0)]
-    spots = spots.sample(10000)
+    spots = pd.read_csv(config['datapath'] + prefix + '_spots.csv')
     render = KDE(spots).get_kde(sigma=2.0)
-    
+
     if plot:
         fig,ax=plt.subplots()
         ax.imshow(render,cmap='gray',vmin=0,vmax=0.2)
@@ -43,5 +30,5 @@ for n,prefix in enumerate(prefixes):
         ax.set_xticks([]); ax.set_yticks([])
 
         plt.show()
-    
-    imsave(config['opath']+prefix+'-kde.tif',render)
+
+    imsave(config['analpath']+prefix+'-kde.tif',render)
--- miniSMLM/psf/psf2d/opt2d.py
+++ miniSMLM/psf/psf2d/opt2d.py
@@ -7,6 +7,88 @@ from .ill2d import *
 from .jac2d import *
 from .hess2d import *
 
+class MLE2D:
+    def __init__(self,theta0,adu,config,theta_gt=None):
+       self.theta0 = theta0
+       self.theta_gt = theta_gt
+       self.adu = adu
+       self.cmos_params = [config['eta'],config['texp'],
+                            np.load(config['gain'])['arr_0'],
+                            np.load(config['offset'])['arr_0'],
+                            np.load(config['var'])['arr_0']]
+
+    def show(self,theta0,theta):
+        fig,ax = plt.subplots(figsize=(4,4))
+        ax.imshow(self.adu,cmap='gray')
+        ax.scatter(theta0[0],theta0[1],color='red',label='raw')
+        ax.scatter(theta[0],theta[1],color='blue',label='fit')
+        ax.legend()
+        plt.tight_layout()
+
+    def plot(self,thetat,iters):
+        fig,ax = plt.subplots(1,4,figsize=(10,2))
+        ax[0].plot(thetat[:,0])
+        if self.theta_gt is not None:
+            ax[0].hlines(y=self.theta_gt[0],xmin=0,xmax=iters,color='red')
+        ax[0].set_xlabel('Iteration')
+        ax[0].set_ylabel('x')
+        ax[1].plot(thetat[:,1])
+        if self.theta_gt is not None:
+            ax[1].hlines(y=self.theta_gt[1],xmin=0,xmax=iters,color='red')
+        ax[1].set_xlabel('Iteration')
+        ax[1].set_ylabel('y')
+        ax[2].plot(thetat[:,2])
+        if self.theta_gt is not None:
+            ax[2].hlines(y=self.theta_gt[2],xmin=0,xmax=iters,color='red')
+        ax[2].set_xlabel('Iteration')
+        ax[2].set_ylabel(r'$\sigma$')
+        ax[3].plot(thetat[:,3])
+        if self.theta_gt is not None:
+            ax[3].hlines(y=self.theta_gt[3],xmin=0,xmax=iters,color='red')
+        ax[3].set_xlabel('Iteration')
+        ax[3].set_ylabel(r'$N_{0}$')
+        plt.tight_layout()
+
+    def get_errors(self,theta):
+        """This doesn't seem to work correctly"""
+        hess = hessiso_auto2d(theta,self.adu,self.cmos_params)
+        try:
+            errors = np.sqrt(np.diag(inv(hess)))
+        except:
+            errors = np.empty((4,))
+            errors[:] = np.nan
+
+        return errors
+
+    def optimize(self,max_iters=1000,lr=None,plot_fit=False,tol=1e-8):
+        if plot_fit:
+           thetat = []
+        if lr is None:
+           lr = np.array([0.001,0.001,0,0])
+        loglike = []
+        theta = np.zeros_like(self.theta0)
+        theta += self.theta0
+        niters = 0
+        converged = False
+        while niters < max_iters:
+            niters += 1
+            loglike.append(isologlike2d(theta,self.adu,self.cmos_params))
+            jac = jaciso2d(theta,self.adu,self.cmos_params)
+            theta = theta - lr*jac
+            if plot_fit:
+                thetat.append(theta)
+            dd = lr[:-1]*jac[:-1]
+            if np.all(np.abs(dd) < tol):
+                converged = True
+                break
+        if plot_fit:
+            self.plot(np.array(thetat),niters)
+            plt.show()
+
+
+        return theta, loglike, converged
+
+
 class IsoLogLikelihood:
     def __init__(self,func,cmos_params):
         self.func = func
@@ -88,84 +170,3 @@ class MLE2D_MCMC:
 
         samples = self.metropolis(theta,iters=mcmc_iters,tburn=tburn,prop_cov=prop_cov,beta=beta,plot_mcmc=plot_mcmc) #seed MH at the MLE
         return theta, loglike, samples
-
-class MLE2D:
-    def __init__(self,theta0,adu,config,theta_gt=None):
-       self.theta0 = theta0
-       self.theta_gt = theta_gt
-       self.adu = adu
-       self.cmos_params = [config['eta'],config['texp'],
-                            np.load(config['gain'])['arr_0'],
-                            np.load(config['offset'])['arr_0'],
-                            np.load(config['var'])['arr_0']]
-
-    def show(self,theta0,theta):
-        fig,ax = plt.subplots(figsize=(4,4))
-        ax.imshow(self.adu,cmap='gray')
-        ax.scatter(theta0[0],theta0[1],color='red',label='raw')
-        ax.scatter(theta[0],theta[1],color='blue',label='fit')
-        ax.legend()
-        plt.tight_layout()
-
-    def plot(self,thetat,iters):
-        fig,ax = plt.subplots(1,4,figsize=(10,2))
-        ax[0].plot(thetat[:,0])
-        if self.theta_gt is not None:
-            ax[0].hlines(y=self.theta_gt[0],xmin=0,xmax=iters,color='red')
-        ax[0].set_xlabel('Iteration')
-        ax[0].set_ylabel('x')
-        ax[1].plot(thetat[:,1])
-        if self.theta_gt is not None:
-            ax[1].hlines(y=self.theta_gt[1],xmin=0,xmax=iters,color='red')
-        ax[1].set_xlabel('Iteration')
-        ax[1].set_ylabel('y')
-        ax[2].plot(thetat[:,2])
-        if self.theta_gt is not None:
-            ax[2].hlines(y=self.theta_gt[2],xmin=0,xmax=iters,color='red')
-        ax[2].set_xlabel('Iteration')
-        ax[2].set_ylabel(r'$\sigma$')
-        ax[3].plot(thetat[:,3])
-        if self.theta_gt is not None:
-            ax[3].hlines(y=self.theta_gt[3],xmin=0,xmax=iters,color='red')
-        ax[3].set_xlabel('Iteration')
-        ax[3].set_ylabel(r'$N_{0}$')
-        plt.tight_layout()
-
-    def get_errors(self,theta):
-        """This doesn't seem to work correctly"""
-        hess = hessiso_auto2d(theta,self.adu,self.cmos_params)
-        try:
-            errors = np.sqrt(np.diag(inv(hess)))
-        except:
-            errors = np.empty((4,))
-            errors[:] = np.nan
-
-        return errors
-
-    def optimize(self,max_iters=1000,lr=None,plot_fit=False,tol=1e-8):
-        if plot_fit:
-           thetat = []
-        if lr is None:
-           lr = np.array([0.001,0.001,0,0])
-        loglike = []
-        theta = np.zeros_like(self.theta0)
-        theta += self.theta0
-        niters = 0
-        converged = False
-        while niters < max_iters:
-            niters += 1
-            loglike.append(isologlike2d(theta,self.adu,self.cmos_params))
-            jac = jaciso2d(theta,self.adu,self.cmos_params)
-            theta = theta - lr*jac
-            if plot_fit:
-                thetat.append(theta)
-            dd = lr[:-1]*jac[:-1]
-            if np.all(np.abs(dd) < tol):
-                converged = True
-                break
-        if plot_fit:
-            self.plot(np.array(thetat),niters)
-            plt.show()
-
-
-        return theta, loglike, converged
