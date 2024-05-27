dff09db62243902e002c897c5fcccac7a8d7b273
clayton <cwseitz@iu.edu>
2024-05-27 15:06:53 -0400

    update

--- miniSMLM/psf/psf2d/crb2d.py
+++ miniSMLM/psf/psf2d/crb2d.py
@@ -3,18 +3,18 @@ from numpy.linalg import inv
 from .psf2d import *
 import matplotlib.pyplot as plt
 
-def crlb2d(theta,cmos_params):
+def crlb2d(theta,sigma,cmos_params):
     ntheta = len(theta)
     eta,texp,gain,offset,var = cmos_params
     nx,ny = offset.shape
-    x0,y0,sigma,N0 = theta
+    x0,y0,N0 = theta
     alpha = np.sqrt(2)*sigma
     x = np.arange(0,nx); y = np.arange(0,ny)
     X,Y = np.meshgrid(x,y,indexing='ij')
     lam = lamx(X,x0,sigma)*lamy(Y,y0,sigma)
     i0 = gain*eta*texp*N0
     muprm = i0*lam + var
-    J = jac1(X,Y,theta,cmos_params)
+    J = jac1(X,Y,theta,sigma,cmos_params)
     I = np.zeros((ntheta,ntheta))
     for n in range(ntheta):
        for m in range(ntheta):
